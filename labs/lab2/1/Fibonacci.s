bd7cff39650ce2e53ef5411e7fe321e7a787ae35
shueja-personal <j.a.shue@outlook.com>
2022-07-21 20:57:43 -0700

    optimizing fibonacci alg

--- labs/lab2/1/Fibonacci.s
+++ labs/lab2/1/Fibonacci.s
@@ -8,37 +8,33 @@
 	.global fibonacci
 	.type   fibonacci, %function  
 
+fibonacci:                  @ int fibonacci(int r0) {
+	str     fp, [sp, -4]!   @   save caller frame pointer
+    add     fp, sp, 0       @   establish our frame pointer
 
+    mov     r1, 0           @   int r1 = 1;
+    mov     r2, 1           @   int r2 = 0;
+    movs    r3, r0          @   int n = r0; // n stored in r3, add s flag to set conditions for r3
 
-fibonacci:
-	str     fp, [sp, -4]!   @ save caller frame pointer
-    add     fp, sp, 0       @ establish our frame pointer
-
-    mov     r1, 0           @ int r1 = 1;
-    mov     r2, 1           @ int r2 = 0;
-    movs    r3, r0          @ int n = r0; // n stored in r3
-L1:                         @   while(n > 1) {
-    cmp r3,2                 
+L1: 
+    cmp r3,2                @   while (n > 1) {     
     bmi L1x
-    add r1, r2, r1          @		r1 = r2+r1;
-    add r2, r1, r2          @		r2 = r1+r2;
-    sub r3, 2               @		n-=2;
-    b L1                    @		
-L1x:                        @	}
+    add r1, r2, r1          @	    r1 = r2 + r1;
+    add r2, r1, r2          @	    r2 = r1 + r2;
+    sub r3, 2               @	    n-=2;
+    b L1                    @   }	
+L1x:                        @
     teq r3,0
-    bne L2                  @	if(n == 0) {
-    mov r0, r1              @		r0 = r1;
-L2:                         @	}
+    @bne L2                  @   if (n == 0) {
+    moveq r0, r1              @	    r0 = r1;
+L2:                         @   }
     teq r3,1                
-    bne L3                  @	if(n == 1) {
-    mov r0, r2              @		r0 = r2;
-L3:                         @	}
+    @bne L3                  @   if (n == 1) {
+    moveq r0, r2              @	    r0 = r2;
+L3:                         @   }
                             @	return r0;
-    beq     cleanup         @
-
-cleanup:
+                            @   
+cleanup:                    @ }
     sub     sp, fp, 0       @ restore stack pointer
     ldr     fp, [sp], 4     @ restore caller's frame pointer
     bx      lr              @ back to caller
-
-
