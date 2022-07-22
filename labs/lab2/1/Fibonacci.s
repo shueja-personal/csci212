93a01fb9067e0d79846df593729f1455eb712e34
shueja-personal <j.a.shue@outlook.com>
2022-07-21 21:09:02 -0700

    moving to assembly fib io

--- labs/lab2/1/Fibonacci.s
+++ labs/lab2/1/Fibonacci.s
@@ -8,33 +8,30 @@
 	.global fibonacci
 	.type   fibonacci, %function  
 
-fibonacci:                  @ int fibonacci(int r0) {
-	str     fp, [sp, -4]!   @   save caller frame pointer
-    add     fp, sp, 0       @   establish our frame pointer
+fibonacci:                  @ int fibonacci(int num) {
+	str     fp, [sp, -4]!   @   // save caller frame pointer
+    add     fp, sp, 0       @   // establish our frame pointer
 
-    mov     r1, 0           @   int r1 = 1;
-    mov     r2, 1           @   int r2 = 0;
-    movs    r3, r0          @   int n = r0; // n stored in r3, add s flag to set conditions for r3
-
+    mov     r1, 0           @   int r1 = 0;
+    mov     r2, 1           @   int r2 = 1;
+    movs    r3, r0          @   int n = num; // n stored in r3, add s flag to set conditions for r3
+                            @   int return; //stored in r0
 L1: 
-    cmp r3,2                @   while (n > 1) {     
+    cmp r3, 2               @   while (n > 1) {     
     bmi L1x
     add r1, r2, r1          @	    r1 = r2 + r1;
-    add r2, r1, r2          @	    r2 = r1 + r2;
+    add r2, r1, r2          @	    r2 = r1 + r2; // Fill r1 and r2 with the next two fibonacci numbers.
     sub r3, 2               @	    n-=2;
     b L1                    @   }	
 L1x:                        @
-    teq r3,0
-    @bne L2                  @   if (n == 0) {
-    moveq r0, r1              @	    r0 = r1;
-L2:                         @   }
-    teq r3,1                
-    @bne L3                  @   if (n == 1) {
-    moveq r0, r2              @	    r0 = r2;
-L3:                         @   }
-                            @	return r0;
-                            @   
+    teq r3,0                @   if (n == 0) {
+    moveq r0, r1            @	    return = r1;
+                            @   }                
+    teq r3,1                @   if (n == 1) {
+    moveq r0, r2            @	    return = r2;
+                            @   }
+                            @ 	return r0;  
 cleanup:                    @ }
-    sub     sp, fp, 0       @ restore stack pointer
-    ldr     fp, [sp], 4     @ restore caller's frame pointer
-    bx      lr              @ back to caller
+    sub     sp, fp, 0       @ // restore stack pointer
+    ldr     fp, [sp], 4     @ // restore caller's frame pointer
+    bx      lr              @ // back to caller
--- labs/lab2/1/main.c
+++ labs/lab2/1/main.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include "fib.h"
 
+//gcc -I. main.c fib.c Fibonacci.s; ./a.out
+
 int main() {
-	//int n = 0;
-	// printf("Enter Fibonacci Term:\n");
-	// scanf("%d", &n);
-	printf("%d\n", fibonacci(26));
-	printf("%d\n", fibonacci(0));
-	int n=-5;
-	int m=-1;
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=0;
-	m=0;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=1;
-	m=1;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=2;
-	m=1;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=3;
-	m=2;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=4;
-	m=3;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=5;
-	m=5;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
-	n=6;
-	m=8;	
-	printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	int n = 0;
+	printf("Enter Fibonacci Term:\n");
+	scanf("%d", &n);
+	// printf("%d\n", fibonacci(26));
+	// printf("%d\n", fibonacci(0));
+	// int n=-5;
+	// int m=-1;
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=0;
+	// m=0;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=1;
+	// m=1;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=2;
+	// m=1;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=3;
+	// m=2;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=4;
+	// m=3;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=5;
+	// m=5;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
+	// n=6;
+	// m=8;	
+	// printf("the %dth Fibonacci number is %d, expected %d, equal: %i\n", n, fibonacci(n), m, fibonacci(n)==m);
 }
